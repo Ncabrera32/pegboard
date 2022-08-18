b7a4e79a21df3532f243ed9a499d18f1c2a4131d
Ncabrera32 <nathanielcabrera88@yahoo.com>
2022-08-17 23:13:39 -0600

    Had to change code entirely wasn't working the way i intended on first go compared 1st code to test code and test code turned out easier to get to work. still some bugs that need to be fixed along with better css.

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
-function makeRows(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
-      let cell = document.createElement("div");
-      //cell.innerText = (c + 1);
-      container.appendChild(cell).className = "grid-item";
-    };
-  };
-  
-  makeRows(16, 16);
-  
-  var colors = ['red', 'black', 'green', 'black', 'black', 'black', 'black', 'black', 'black', 'black','black', 'black', 'black', 'black','black', 'black'];
-  var boxes = document.querySelectorAll(".grid-item");
-  var button = document.querySelector("button");
-  
-  button.addEventListener("click", function () {
-    for (i = 0; i < boxes.length; i++) {
-      // Pick a random color from the array 'colors'.
-      boxes[i].style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+let color = "black";
+let click = true;
+let colors = ['red', 'black', 'black', 'black', 'black', 'black', 'black', 'black', 
+'black', 'black', 'black', 'black', 'black', 'black', 'green'];
+
+function populateBoard(size){
+let board = document.querySelector(".board");
+let squares = board.querySelectorAll("div");
+squares.forEach(div => div.remove());
+board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+let amount = size * size
+for(let i = 0; i < amount; i++) {
+let square = document.createElement("div");
+square.addEventListener("click", colorSquare);
+    square.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+    board.insertAdjacentElement("beforeend", square);
+    board.appendChild(square).className = "target";
+}
+}
+
+populateBoard(16);
+
+function changeSize(input) {
+    if (input >= 6 && input <= 23) {
+      document.querySelector(".error").style.display = "none";
+      populateBoard(input);
+    } else {
+      document.querySelector(".error").style.display = "flex";
     }
-  });
-  
-  button.style.cursor = "pointer";
+  }
+
+function colorSquare(){
+if (click) {
+    if (color === "random"){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = color;
+    }
+}
+}
+
+function changeColor(choice) {
+    color = choice;
+}
+
+function resetBoard() {
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach(div => div.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]);
+}
